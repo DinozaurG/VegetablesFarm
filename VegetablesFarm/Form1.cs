8afd78eca9c13fdb539eefe7edd1fb71549e835c
Dinozaur_G <karnah@mail.ru>
2020-02-11 11:30:24 +0700

    Овощи посажены. Пока бесплатно

--- VegetablesFarm/Form1.cs
+++ VegetablesFarm/Form1.cs
@@ -16,5 +16,53 @@ namespace VegetablesFarm
         {
             InitializeComponent();
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            if (cb.Checked)
+            {
+                StartGrow(cb);
+            }
+            else
+            {
+                CutGrow(cb);
+            }
+        }
+        internal void StartGrow(CheckBox cb)
+        {
+            cb.BackColor = Color.Black;
+        }
+        internal void CutGrow(CheckBox cb)
+        {
+            cb.BackColor = Color.White;
+        }
+        internal void nextState(CheckBox cb)
+        {
+            Color c = Color.White;
+            if (cb.BackColor == Color.Black)
+            {
+                cb.BackColor = Color.Green;
+            }
+            else if (cb.BackColor == Color.Green)
+            {
+                cb.BackColor = Color.Yellow;
+            }
+            else if (cb.BackColor == Color.Yellow)
+            {
+                cb.BackColor = Color.Red;
+            }
+            else if (cb.BackColor == Color.Red)
+            {
+                cb.BackColor = Color.Brown;
+            }
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            foreach(CheckBox cb in panel1.Controls)
+            {
+                nextState(cb);
+            }
+        }
     }
 }
